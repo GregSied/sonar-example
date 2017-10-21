bd4b4461efca61235f5bcbe8907563cce3770eb7
Marcin Król <marcinkrol1024@gmail.com>
2017-10-21 11:00:45 +0200

    Add sonar to pom.xml

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,10 +3,24 @@ package test;
 public class Test {
 
   public int foo(String bar) {
+    int a = 2;
+    int b = 3;
+    int c = a + b;
     try {
-      return Integer.parseInt(bar);
+      return Integer.parseInt("2");
     } catch (Exception e) {
-      return 0;
+      return c;
+    }
+  }
+
+  public int baz(String bar) {
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    try {
+      return Integer.parseInt("2");
+    } catch (Exception e) {
+      return c;
     }
   }
 }
